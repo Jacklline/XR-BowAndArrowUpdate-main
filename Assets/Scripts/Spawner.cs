1e57c0e5873add443eaad06a42022ecc047c5cf3
Esther W <Fizzystar@hotmail.com>
2022-10-11 11:50:21 +1100

    UI works with Grab
    Got the UI working when grabbing the bow at the begining of t he game.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -11,17 +11,22 @@ public class Spawner : MonoBehaviour
     private float fcounter;
     public float fSpawnDelay = 2;
     public float fMinSpawnDelay = 1;
+    
+    
 
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.childCount < iTotalObjectsinWave && fcounter <= 0) 
+        
+       
+        if (transform.childCount < iTotalObjectsinWave && fcounter <= 0)
         {
             Vector3 startpos = new Vector3(Random.Range(-fareaofspawnStart, fareaofspawnStart), 0, Random.Range(-fareaofspawnStart, fareaofspawnStart)).normalized * fareaofspawnStart;
             GameObject objecttospawn = Instantiate(FloatingObjects[Random.Range(0, FloatingObjects.Count)], startpos + transform.position, transform.rotation, transform);
             fcounter += Random.Range(fMinSpawnDelay, fSpawnDelay);
         }
+        
 
         fcounter -= Time.deltaTime;
         Windforce += Random.insideUnitSphere;
